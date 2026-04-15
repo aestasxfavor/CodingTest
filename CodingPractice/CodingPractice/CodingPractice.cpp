a5ad6d0521ebfe8060a382a4647c399dc3e1e8c4
aestasxfavor <pstar0819@naver.com>
2026-04-15 13:43:18 +0900

    (2026-04-15) 코딩테스트
    - 2문제 풀려고 했으나 문제 하나가 시간을 투자해야하는 문제였기에 1문제만 풀었음

--- CodingPractice/CodingPractice/CodingPractice.cpp
+++ CodingPractice/CodingPractice/CodingPractice.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
 {
-#pragma region Coiding Test 2026_04_05
+#pragma region Coding Test 2026_04_05
 	/*
 	백준 18108번: 1998년생인 내가 태국에서는 2541년생?!
 
@@ -38,125 +38,155 @@ int main()
 #pragma endregion
 
 #pragma region Coding Test 2026_04_06
-	/*
-	* 백준 2588번: 곱셈
-	int a;
-	int b;
-	
-	cin >> a;
-	cin >> b;
-	
-	int c = b % 10;
-	int d = (b / 10) % 10;
-	int e = b / 100;
-	
-	cout << a * c << endl;
-	cout << a * d << endl;
-	cout << a * e << endl;
-	cout << a * b << endl;
-	*/
+/*
+* 백준 2588번: 곱셈
+int a;
+int b;
+
+cin >> a;
+cin >> b;
+
+int c = b % 10;
+int d = (b / 10) % 10;
+int e = b / 100;
+
+cout << a * c << endl;
+cout << a * d << endl;
+cout << a * e << endl;
+cout << a * b << endl;
+*/
 #pragma endregion
 
 #pragma region Coding Test 2026_04_12
-	/*
-	백준: 11382번 꼬마 정민
-	
-	long long a, b, c, sum;
-	
-	cin >> a >> b >> c;
-	
-	sum = a + b + c;
-	
-	cout << sum << endl;
-	
-	*/
+/*
+백준: 11382번 꼬마 정민
+
+long long a, b, c, sum;
+
+cin >> a >> b >> c;
+
+sum = a + b + c;
+
+cout << sum << endl;
+
+*/
 #pragma endregion
 
 #pragma region Coding Test 2026_04_13
-	/*
-	백준: 10172번 고양이
-	cout << "\\    /\\" << endl;
-	cout << " )  ( ')" << endl;
-	cout << "(  /  )" << endl;
-	cout << " \\(__)|" << endl;
-	
-	*/
-	
-	/*
-	백준 10172번 강아지
-	cout << "|\\_/|" << endl;
-	cout << "|q p|   /}" << endl;
-	cout << "( 0 )\"\"\"\\" << endl;
-	cout << "|\"^\"`    |" << endl;
-	cout << "||_/=\\\\__|" << endl;
-	
-	*/
-	#pragma endregion
+/*
+백준: 10172번 고양이
+cout << "\\    /\\" << endl;
+cout << " )  ( ')" << endl;
+cout << "(  /  )" << endl;
+cout << " \\(__)|" << endl;
+
+*/
+
+/*
+백준 10172번 강아지
+cout << "|\\_/|" << endl;
+cout << "|q p|   /}" << endl;
+cout << "( 0 )\"\"\"\\" << endl;
+cout << "|\"^\"`    |" << endl;
+cout << "||_/=\\\\__|" << endl;
+
+*/
+#pragma endregion
 
 #pragma region Coding Test 2026-04-14
-	/*
-	백준 1330번 두 수 비교하기
-	
-	int a, b;
-	cin >> a >> b;
-	
-	if (a > b)
+/*
+백준 1330번 두 수 비교하기
+
+int a, b;
+cin >> a >> b;
+
+if (a > b)
+{
+	cout << ">";
+}
+else if (a < b)
+{
+	cout << "<";
+}
+else
+{
+	cout << "==";
+}
+*/
+
+/*
+백준 9498 시험 성적
+	int a;
+	cin >> a;
+
+	if (a >= 90)
 	{
-		cout << ">";
+		cout << "A";
 	}
-	else if (a < b)
+	else if (a >= 80)
 	{
-		cout << "<";
+		cout << "B";
+	}
+	else if (a >= 70)
+	{
+		cout << "C";
+	}
+	else if (a >= 60)
+	{
+		cout << "D";
 	}
 	else
 	{
-		cout << "==";
+		cout << "F";
 	}
-	*/
-	
-	/*
-	백준 9498 시험 성적
-		int a;
-		cin >> a;
-	
-		if (a >= 90)
-		{
-			cout << "A";
-		}
-		else if (a >= 80)
-		{
-			cout << "B";
-		}
-		else if (a >= 70)
-		{
-			cout << "C";
-		}
-		else if (a >= 60)
-		{
-			cout << "D";
-		}
-		else
-		{
-			cout << "F";
-		}
-	*/
-	
-	/*
-	백준 2753번 윤년
-	
-		int a;
-		cin >> a;
-	
-		if (a % 4 == 0 && a % 100 != 0 || a % 400 == 0)
-		{
-			cout << "1";
-		}
-		else
-		{
-			cout << "0";
-		}
-	*/
+*/
+
+/*
+백준 2753번 윤년
+
+	int a;
+	cin >> a;
+
+	if (a % 4 == 0 && a % 100 != 0 || a % 400 == 0)
+	{
+		cout << "1";
+	}
+	else
+	{
+		cout << "0";
+	}
+*/
 #pragma endregion
 
+#pragma region Coding Test 2026-04-15
+/*
+* 백준 14681 사분면 고르기
+	int a, b;
+
+	cin >> a;
+	cin >> b;
+
+	if (a > 0 && b > 0)
+	{
+		cout << "1";
+	}
+	else if (a < 0 && b > 0)
+	{
+		cout << "2";
+	}
+	else if (a < 0 && b < 0)
+	{
+		cout << "3";
+	}
+	else
+	{
+		cout << "4";
+	}
+*/
+#pragma endregion
+
+
+
+	
 
 }
